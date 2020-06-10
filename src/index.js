b81fa11b0852f115a19526927d165f7ad6c1892b
Daryl <mrdaryldauphin@gmail.com>
2020-06-10 19:40:21 -0400

    working on skills and bio

--- src/index.js
+++ src/index.js
@@ -323,7 +323,7 @@ Send Message      </Button>
 <Paper className={classes.first} boxShadow={3} elevation={3} variant="outlined">
   
 <header>
-  <h1>BIO / SKILLS</h1>
+  <h1>SKILLS</h1>
 </header>
 
   <div className={classes.bio}>
@@ -335,8 +335,180 @@ Send Message      </Button>
 
 
     </div>
-<div>Skills</div>
-
+<div class="wrap">
+		<div class="comp">
+			<div class="monitor">
+				<div class="mid">
+					<div class="site">
+						<div class="topbar">
+							<div class="cerrar">
+								<div class="circl"></div>
+								<div class="circl"></div>
+								<div class="circl"></div>
+							</div>
+						</div>
+						<div class="inhead">
+							<div class="mid">
+								<div class="item"></div>
+							</div>
+							<div class="mid txr">
+								<div class="item"></div>
+								<div class="item"></div>
+								<div class="item"></div>
+								<div class="item"></div>
+							</div>
+						</div>
+						<div class="inslid">
+							
+						</div>
+						<div class="incont">
+							<div class="item"></div>
+							<div class="item"></div>
+							<div class="item"></div>
+							<div class="item"></div>
+							<div class="wid">
+								<div class="itwid">
+									<div>
+										<div class="contfoot"></div>
+									</div>
+								</div>
+								<div class="itwid">
+									<div>
+										<div class="contfoot"></div>
+									</div>
+								</div>
+								<div class="itwid">
+									<div>
+										<div class="contfoot"></div>
+									</div>
+								</div>
+								<div class="clearfix"></div>
+							</div>
+							<div class="infoot">
+								
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="mid codigo">
+					<div class="line">
+						<div class="item var"></div>
+						<div class="item cont"></div>
+						<div class="clearfix"></div>
+					</div>
+					<div class="line">
+						<div class="item min var"></div>
+						<div class="item min fun"></div>
+						<div class="clearfix"></div>
+					</div>
+					<div class="line">
+						<div class="item min var"></div>
+						<div class="clearfix"></div>
+					</div>
+					<div class="line">
+						<div class="item var"></div>
+						<div class="item atr"></div>
+						<div class="item cont"></div>
+						<div class="clearfix"></div>
+					</div>
+					<div class="line tab1">
+						<div class="item min atr"></div>
+						<div class="item lrg fun"></div>
+						<div class="item min fun"></div>
+						<div class="item lrg cont"></div>
+						<div class="clearfix"></div>
+					</div>
+					<div class="line tab1">
+						<div class="item lrg atr"></div>
+						<div class="item min fun"></div>
+						<div class="item min cont"></div>
+						<div class="clearfix"></div>
+					</div>
+					<div class="line tab1">
+						<div class="item atr"></div>
+						<div class="item min fun"></div>
+						<div class="item atr"></div>
+						<div class="clearfix"></div>
+					</div>
+					<div class="line tab1">
+						<div class="item min atr"></div>
+						<div class="item min cont"></div>
+						<div class="item lrg atr"></div>
+						<div class="item  fun"></div>
+						<div class="clearfix"></div>
+					</div>
+					<div class="line tab1">
+						<div class="item min atr"></div>
+						<div class="item lrg fun"></div>
+						<div class="item lrg cont"></div>
+						<div class="item min fun"></div>
+						<div class="clearfix"></div>
+					</div>
+					<div class="line tab1">
+						<div class="item min var"></div>
+						<div class="clearfix"></div>
+					</div>
+					<div class="line tab1">
+						<div class="item min var"></div>
+						<div class="clearfix"></div>
+					</div>
+					<div class="line tab2">
+						<div class="item min var"></div>
+						<div class="clearfix"></div>
+					</div>
+					<div class="line tab2">
+						<div class="item min atr"></div>
+						<div class="item min fun"></div>
+						<div class="clearfix"></div>
+					</div>
+					<div class="line tab3">
+						<div class="item min atr"></div>
+						<div class="item min fun"></div>
+						<div class="item lrg fun"></div>
+						<div class="item lrg cont"></div>
+						<div class="clearfix"></div>
+					</div>
+					<div class="line tab3">
+						<div class="item min atr"></div>
+						<div class="item min fun"></div>
+						<div class="item lrg atr"></div>
+						<div class="item lrg cont"></div>
+						<div class="clearfix"></div>
+					</div>
+					<div class="line tab4">
+						<div class="item min fun"></div>
+						<div class="item lrg atr"></div>
+						<div class="clearfix"></div>
+					</div>
+					<div class="line tab1">
+						<div class="item atr"></div>
+						<div class="item var"></div>
+						<div class="item cont"></div>
+						<div class="clearfix"></div>
+					</div>
+					<div class="line tab3">
+						<div class="item min var"></div>
+						<div class="clearfix"></div>
+					</div>
+					<div class="line tab4">
+						<div class="item min atr"></div>
+						<div class="item min fun"></div>
+						<div class="item lrg atr"></div>
+						<div class="item lrg cont"></div>
+						<div class="clearfix"></div>
+					</div>
+					<div class="line">
+						<div class="item min var"></div>
+						<div class="clearfix"></div>
+					</div>
+					
+				</div>
+			</div>
+			<div class="base">
+				
+			</div>
+		</div>
+	</div>
   </Paper> 
 {/* <div class="BIO">
   
